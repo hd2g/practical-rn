074b9c661484085f26e4e27be0cd55464bc1af72
hd2g <nagisa.159s.8673.trap@gmail.com>
2023-06-26 19:52:25 +0900

    feat: write hello world server for the time being

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,8 +1,43 @@
+{-# language OverloadedStrings #-}
+{-# language DeriveGeneric #-}
+{-# language DataKinds #-}
+{-# language TypeOperators #-}
+
 module Main where
 
 import qualified MyLib (someFunc)
 
+import GHC.Generics (Generic)
+
+import Data.Aeson
+import Data.Text (Text)
+
+import Servant
+
+import Network.Wai.Handler.Warp as Warp
+
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  Warp.run 8080 app
+
+app :: Application
+app = serve helloApi server
+
+server :: Server HelloAPI
+server = hello :<|> user where
+  hello = return "Hello world"
+  user n a = return $ User n a
+
+data User = User
+  { name :: Text
+  , age :: Int
+  } deriving (Eq, Show, Read, Generic)
+
+instance FromJSON User
+instance ToJSON User
+
+type HelloAPI = Get '[PlainText] Text
+            :<|> "user" :> Capture "name" Text :> Capture "age" Int :> Get '[JSON] User
+
+helloApi :: Proxy HelloAPI
+helloApi = Proxy
